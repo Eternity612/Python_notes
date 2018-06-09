98a5a501329e230b5af0596279a0beb8d4df9db1
xyf186 <790577657@qq.com>
2018-06-09 23:14:30 +0800

    面向对象2

---
+++ 01基础/08-面向对象-2/15-类属性、实例属性.py
@@ -0,0 +1,14 @@
+class Tool(object):
+
+    #类属性
+    num = 0
+
+    #方法
+    def __init__(self,new_name):
+        self.name = new_name
+
+
+
+tool1 = Tool("铁锹")
+tool2 = Tool("铲子")
+tool3 = Tool("水桶")
